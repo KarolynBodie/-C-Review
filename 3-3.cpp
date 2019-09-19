58a19513fa17365ba71f864ba0fa615255f865bb
KarolynBodie <54687648+KarolynBodie@users.noreply.github.com>
2019-09-19 11:30:23 -0400

    Update 3-3.cpp

--- 3-3.cpp
+++ 3-3.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <cmath>    //  <<Remember to include
+#include <iomanip>  //   <<Remember to include
 
 using namespace std;
 
 int main() {
  
-    double kg;
+    double kg;                //Assign Variables
     double pound = 2.2;
     double convert;
     
     
     cout << "Enter the weight of a person in kilograms and hit enter: " << endl;
     cin >> kg;
-    convert = kg * pound;
+    convert = kg * pound;   //  Create Algorithm
     
       cout << "Weight of a person in kilograms converted to pounds: " << fixed << setprecision(2) << convert << endl;
-    
+                                                //*Remember to include* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     return 0;
 }
