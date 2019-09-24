297dc336abd420982a8fb8d71595ce3e21213fa1
KarolynBodie <54687648+KarolynBodie@users.noreply.github.com>
2019-09-23 22:55:02 -0400

    Update 3-3.cpp

--- 3-3.cpp
+++ 3-3.cpp
@@ -1,3 +1,9 @@
+/*Instructions
+Write a program that prompts the user to enter the weight of a person in kilograms
+and outputs the equivalent weight in pounds (Note that 1 kilogram equals 2.2 pounds). 
+Format your output with two decimal places. */
+
+
 #include <iostream>
 #include <cmath>    //  <<Remember to include
 #include <iomanip>  //   <<Remember to include
